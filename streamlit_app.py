f9cc4488dcef52cd4fcc21e9d908863607a69e49
venkatb2k <venkatb2k@gmail.com>
2023-03-29 15:10:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
+
+import streamlit as stimport pandas as pdimport numpy as npdf= pd.DataFrame(    np.random.randn(10, 2),    columns=['x', 'y'])st.bar_chart(df)
