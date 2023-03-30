50969e63ddbfce832358269bc68e1994843590c4
venkatb2k <venkatb2k@gmail.com>
2023-03-29 21:47:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,34 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
 
 df= pd.DataFrame(    np.random.randn(10, 2),    columns=['x', 'y'])
 st.bar_chart(df)
+
+
+
+
+# Create a sample dataframe
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'Alice', 'Bob', 'Charlie', 'Alice', 'Bob', 'Charlie'],
+        'Product': ['Apples', 'Apples', 'Apples', 'Oranges', 'Oranges', 'Oranges', 'Pears', 'Pears', 'Pears'],
+        'Sales': [100, 200, 300, 50, 75, 125, 75, 100, 150]}
+df = pd.DataFrame(data)
+
+# Create a pivot table
+pivot = df.pivot_table(values='Sales', index='Name', columns='Product')
+
+# Create a bar chart
+fig, ax = plt.subplots()
+pivot.plot(kind='bar', ax=ax)
+
+# Set chart title and axis labels
+ax.set_title('Sales by Product and Name')
+ax.set_xlabel('Name')
+ax.set_ylabel('Sales')
+
+# Display the chart using Streamlit
+st.pyplot(fig)
